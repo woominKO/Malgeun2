a5f6291b7a261994b4285d341d580915e12d1fa1
woomin422@gmail.com <woomin422@gmail.com>
2024-12-24 17:33:00 +0900

    mongo

--- public/server.js
+++ public/server.js
@@ -1,28 +1,44 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
 const app = express();
 
 app.use(bodyParser.json());
 
-let notices = [];
+const url = 'mongodb://127.0.0.1:27017';
+const dbName = 'noticesDB';
+let db;
 
-app.listen(3000, function() {
-    console.log('listening on 3000');
+MongoClient.connect(url, (err, client) => {
+    if (err) throw err;
+    db = client.db(dbName);
+    console.log(`Connected to database: ${dbName}`);
+
+    app.listen(3000, function() {
+        console.log('Server is running on http://localhost:3000');
+    });
 });
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/notices', (req, res) => {
+app.get('/notices', async (req, res) => {
+    if (!db) {
+        return res.status(500).send('Database not connected');
+    }
+    const notices = await db.collection('notices').find().toArray();
     res.json(notices);
 });
 
-app.post('/notices', (req, res) => {
+app.post('/notices', async (req, res) => {
+    if (!db) {
+        return res.status(500).send('Database not connected');
+    }
     const notice = req.body;
-    notices.push(notice);
+    await db.collection('notices').insertOne(notice);
     res.status(201).json(notice);
 });
