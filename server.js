0ecc894d12fa61d2144e2ae36887ec89134df055
woomin422@gmail.com <woomin422@gmail.com>
2024-12-24 19:54:03 +0900

    마감

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// 정적 파일 제공
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 서버와 브라우저가 연결될 때까지 기다리는 것
+app.listen(3000, function() {
+    console.log('listening on 3000');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
