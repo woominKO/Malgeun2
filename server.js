94f9562a1356851f14777b1d4fe9b435701dc1f7
woominKo <woomin42@naver.com>
2024-12-25 20:00:29 +0900

    서버 정상화

--- server.js
+++ server.js
@@ -1,20 +1,74 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { MongoClient, ServerApiVersion } = require('mongodb'); // ServerApiVersion 가져오기
 const path = require('path');
 const app = express();
+// view engine 설정
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-// 정적 파일 제공
-app.use(express.static(path.join(__dirname, 'public')));
-
-// 서버와 브라우저가 연결될 때까지 기다리는 것
-app.listen(3000, function() {
-    console.log('listening on 3000');
+const uri = "mongodb+srv://woomin422:b9SdJwPrQCvPUX2c@cluster0.nwxfl.mongodb.net/";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
+let quotesCollection;
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname, 'index.html');
-});
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db('게시글');
+    quotesCollection = db.collection('quotes');
+    console.log("Connected to Database");
 
-app.post('/quotes', (req, res) => {
-   console.log('Hellooooooooooooooooo!');
-});
+    app.listen(3000, function() {
+      console.log('listening on 3000');
+    });
+
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    // body-parser
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    
+
+    // sendFile method를 통해 index.html파일로 연결하자
+    app.get('/', (req, res) => {
+      res.sendFile(__dirname + '/index.html');
+    });
+
+    // 공지사항.html 경로에 대한 GET 요청 처리
+    app.get('/notice', (req, res) => {
+      quotesCollection.find().toArray()
+        .then(results => {
+          res.render('notice.ejs', { quotes: results });
+        })
+        .catch(error => console.error(error));
+    });
+    
+
+    app.post('/quotes', (req, res) => {
+      const quote = {
+        title: req.body.title,
+        detail: req.body.detail,
+        date: new Date()
+      };
+      quotesCollection.insertOne(quote)
+        .then(result => {
+          res.redirect('/notice.html');
+        })
+        .catch(error => console.error(error));
+    });
+
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+run().catch(console.dir);
